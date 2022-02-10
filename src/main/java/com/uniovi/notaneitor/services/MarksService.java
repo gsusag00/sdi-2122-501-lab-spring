91669598cf84ae9dde68b883d1913583e313baf7
Jesus Alonso Garcia <jesus.ag00@gmail.com>
2022-02-10 19:52:20 +0100

    SDI-2122-501-2.4-Modelo,acceso a datos simple

--- src/main/java/com/uniovi/notaneitor/services/MarksService.java
+++ src/main/java/com/uniovi/notaneitor/services/MarksService.java
@@ -1,9 +1,12 @@
 package com.uniovi.notaneitor.services;
 
 import com.uniovi.notaneitor.entities.Mark;
+import com.uniovi.notaneitor.repositories.MarksRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import javax.annotation.PostConstruct;
+import java.util.ArrayList;
 import java.util.LinkedList;
 import java.util.List;
 
@@ -13,20 +16,13 @@ import java.util.List;
 @Service
 public class MarksService {
 
-    private List<Mark> marksList = new LinkedList<Mark>();
-
-
-    /**
-     * Es necesario añadir esta anotacion si de verdad queremos que una funcion actue como inicializador.
-     */
-    @PostConstruct
-    public void init() {
-        marksList.add(new Mark(1L,"Ejercicio 1", 10.0));
-        marksList.add(new Mark(2L,"Ejercicio2",9.0));
-    }
+    @Autowired
+    private MarksRepository marksRepository;
 
     public List<Mark> getMarks() {
-        return marksList;
+        List<Mark> marks = new ArrayList<Mark>();
+        marksRepository.findAll().forEach(marks::add);
+        return marks;
     }
 
     /**
@@ -37,19 +33,16 @@ public class MarksService {
      * @return
      */
     public Mark getMark(Long id) {
-        return marksList.stream().filter(mark -> mark.getId().equals(id)).findFirst().get();
+        return marksRepository.findById(id).get();
     }
 
     public void addMark(Mark mark) {
         //Si el id es null le asignamos el último + 1 de la lista.
-        if(mark.getId() == null){
-            mark.setId(marksList.get(marksList.size() - 1).getId() + 1);
-        }
-        marksList.add(mark);
+        marksRepository.save(mark);
     }
 
     public void deleteMark(Long id) {
-        marksList.removeIf(mark->mark.getId().equals(id));
+        marksRepository.deleteById(id);
     }
 
 }
