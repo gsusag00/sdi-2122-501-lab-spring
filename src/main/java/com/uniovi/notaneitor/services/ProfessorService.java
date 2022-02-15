b246084366d9e5602b79979af17cea81fd1e80db
jesusag00 <jesus.ag00@gmail.com>
2022-02-15 09:49:27 +0100

    “SDI-2122-501-sesión2-Ejercicio-complementario2

--- src/main/java/com/uniovi/notaneitor/services/ProfessorService.java
+++ src/main/java/com/uniovi/notaneitor/services/ProfessorService.java
@@ -2,6 +2,9 @@ package com.uniovi.notaneitor.services;
 
 import com.uniovi.notaneitor.entities.Mark;
 import com.uniovi.notaneitor.entities.Professor;
+import com.uniovi.notaneitor.repositories.ProfessorRepository;
+import net.bytebuddy.dynamic.scaffold.MethodGraph;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import javax.annotation.PostConstruct;
@@ -11,30 +14,30 @@ import java.util.List;
 @Service
 public class ProfessorService {
 
-    private List<Professor> professorsList = new LinkedList<>();
+    @Autowired
+    private ProfessorRepository professorRepository;
 
     @PostConstruct
     public void init(){
-        professorsList.add(new Professor(1L, "123456789", "Nombre 1","Apellido 1","categoria 1"));
-        professorsList.add(new Professor(2L, "987654321", "Nombre 2","Apellido 2","categoria 2"));
+        professorRepository.save(new Professor(1L, "123456789", "Nombre 1","Apellido 1","categoria 1"));
+        professorRepository.save(new Professor(2L, "987654321", "Nombre 2","Apellido 2","categoria 2"));
     }
 
     public List<Professor> getProfessors(){
+        List<Professor> professorsList = new LinkedList<>();
+        professorRepository.findAll().forEach(professorsList::add);
         return professorsList;
     }
 
     public Professor getProfessor(Long id){
-        return professorsList.stream().filter(mark->mark.getId().equals(id)).findFirst().get();
+        return professorRepository.findById(id).get();
     }
 
     public void addProfessor(Professor professor){
-        if(professor.getId() == null){
-            professor.setId(professorsList.get(professorsList.size() - 1).getId() + 1);
-        }
-        professorsList.add(professor);
+        professorRepository.save(professor);
     }
 
     public void deleteProfessor(Long id){
-        professorsList.removeIf(mark->mark.getId().equals(id));
+        professorRepository.deleteById(id);
     }
 }
