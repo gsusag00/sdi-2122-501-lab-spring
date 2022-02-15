a80d479c4f1f3fcd74fa693e7bdc6d976ec2895f
jesusag00 <jesus.ag00@gmail.com>
2022-02-15 10:22:34 +0100

    “SDI-2122-501-sesión2-Ejercicio-complementario3

--- src/main/java/com/uniovi/notaneitor/controllers/ProfessorController.java
+++ src/main/java/com/uniovi/notaneitor/controllers/ProfessorController.java
@@ -1,35 +1,58 @@
 package com.uniovi.notaneitor.controllers;
 
+import com.uniovi.notaneitor.entities.Mark;
 import com.uniovi.notaneitor.entities.Professor;
 import com.uniovi.notaneitor.services.ProfessorService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
 
-@RestController
+@Controller
 public class ProfessorController {
 
     @Autowired
     private ProfessorService professorService;
 
     @RequestMapping("/professor/list")
-    public String getList(){
-        return professorService.getProfessors().toString();
+    public String getList(Model model){
+        model.addAttribute("professorList", professorService.getProfessors());
+        return "professor/list";
     }
 
     @RequestMapping(value="/professor/add", method = RequestMethod.POST)
     public String setProfessor(@ModelAttribute Professor professor){
         professorService.addProfessor(professor);
-        return "Professor added succesfully";
+        return "redirect:/professor/list";
+    }
+
+    @RequestMapping("professor/add")
+    public String getProfessor(){
+        return "professor/add";
     }
 
     @RequestMapping("/professor/details/{id}")
-    public String getDetails(@PathVariable Long id){
-        return professorService.getProfessor(id).toString();
+    public String getDetails(Model model, @PathVariable Long id){
+        model.addAttribute("professor", professorService.getProfessor(id));
+        return "professor/details";
     }
 
     @RequestMapping("/professor/delete/{id}")
     public String deleteProfessor(@PathVariable Long id){
         professorService.deleteProfessor(id);
-        return "Professor added succesfully.";
+        return "redirect:/professor/list";
+    }
+
+    @RequestMapping(value="/professor/edit/{id}")
+    public String getEdit(Model model, @PathVariable Long id) {
+        model.addAttribute("professor",professorService.getProfessor(id));
+        return "professor/edit";
+    }
+
+    @RequestMapping(value="/professor/edit/{id}", method=RequestMethod.POST)
+    public String setEdit(@ModelAttribute Professor professor, @PathVariable Long id){
+        professor.setId(id);
+        professorService.addProfessor(professor);
+        return "redirect:/professor/details/"+id;
     }
 }
--- src/main/java/com/uniovi/notaneitor/services/ProfessorService.java
+++ src/main/java/com/uniovi/notaneitor/services/ProfessorService.java
@@ -17,11 +17,12 @@ public class ProfessorService {
     @Autowired
     private ProfessorRepository professorRepository;
 
-    @PostConstruct
+
+/*    @PostConstruct
     public void init(){
         professorRepository.save(new Professor(1L, "123456789", "Nombre 1","Apellido 1","categoria 1"));
         professorRepository.save(new Professor(2L, "987654321", "Nombre 2","Apellido 2","categoria 2"));
-    }
+    }*/
 
     public List<Professor> getProfessors(){
         List<Professor> professorsList = new LinkedList<>();
